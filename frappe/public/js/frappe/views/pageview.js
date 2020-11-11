abda47e78b53a85771890f8b97338b1560ab42ab
Rushabh Mehta <rmehta@gmail.com>
2020-11-11 11:45:15 +0530

    fix(routing-v2): handle pop state and use currentTarget

--- frappe/public/js/frappe/views/pageview.js
+++ frappe/public/js/frappe/views/pageview.js
@@ -39,32 +39,31 @@ frappe.views.pageview = {
 	},
 
 	show: function(name) {
-		if(!name) {
+		if (!name) {
 			name = (frappe.boot ? frappe.boot.home_page : window.page_name);
 
-			if(name === "workspace") {
-				if(!frappe.workspace) {
+			if (name === "workspace") {
+				if (!frappe.workspace) {
 					let page = frappe.container.add_page('workspace');
 					let container = $('<div class="container"></div>').appendTo(page);
 					container = $('<div></div>').appendTo(container);
 
 					frappe.workspace = new Desktop({
 						wrapper: container
-					})
+					});
 				}
-
-				frappe.container.change_to('workspace');
-				frappe.workspace.route();
-				frappe.utils.set_title(__('Desk'));
-				return;
 			}
+			frappe.container.change_to('workspace');
+			frappe.workspace.route();
+			frappe.utils.set_title(__('Home'));
+			return;
 		}
 		frappe.model.with_doctype("Page", function() {
 			frappe.views.pageview.with_page(name, function(r) {
-				if(r && r.exc) {
-					if(!r['403'])
+				if (r && r.exc) {
+					if (!r['403'])
 						frappe.show_not_found(name);
-				} else if(!frappe.pages[name]) {
+				} else if (!frappe.pages[name]) {
 					new frappe.views.Page(name);
 				}
 				frappe.container.change_to(name);
